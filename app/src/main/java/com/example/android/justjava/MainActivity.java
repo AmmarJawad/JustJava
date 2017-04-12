2542d505b57593c4e86f310a26dd8b322293d3ff
Ammar Jawad <ajawad@radiantworlds.com>
2017-04-12 18:08:45 +0100

    95% done

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,7 +8,11 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import static com.example.android.justjava.R.id.chocolate_checkbox;
 
 /**
  * This app displays an order form to order coffee.
@@ -26,17 +30,31 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        EditText nameInput = (EditText) findViewById(R.id.nameField);
+        String name = nameInput.getText().toString();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolate, name));
     }
 
     /**
      * Create a summary of the order
      * @param price refers to calculatePrice method which is quantity * 5
+     * @param addWhippedCream is whether or no the whipped cream checkbox has been checked
+     * @param addChocolate is whether chocolate has been selected in the checkbox
      * @return order summary
      */
-    private String createOrderSummary(int price){
-        String priceMessage = "Name: Ammar J";
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String username){
+        String priceMessage = "Name: " + username;
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank you!";
@@ -45,20 +63,45 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Calculates the price of the order.
+     * @param addChocolate whether or not the user prompted for chocolate.
+     * @param addWhippedCream whether or not the user prompted for whipped cream.
+     *
      * @return total price
      */
 
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // price of a cup of coffee is initialised to $5
+        int basePrice = 5;
+
+        // if a user checks whipped cream checkbox then add + $1 to coffee price.
+        if (addWhippedCream) {
+            basePrice += 1;
+        }
+
+        // if a user checks chocolate checkbox then add + $2 to coffee price.
+        if (addChocolate) {
+            basePrice += 2;
+        }
+
+        // return the quantity multiplied by the unit price.
+        return quantity * basePrice;
     }
 
     public void increment(View view) {
-        quantity = quantity + 1;
+        if (quantity == 100) {
+            return;
+        }
+
+        quantity += 1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        quantity = quantity - 1;
+        if (quantity == 1) {
+            return;
+        }
+
+        quantity -= 1;
         displayQuantity(quantity);
     }
 
